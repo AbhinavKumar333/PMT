083e1b1bbef1fd20393c4034f1869124f88c0359
abhinavkumar333 <abhinav333.ak@outlook.com>
2019-10-29 17:29:22 +0530

    Added PMT User

---
+++ PMT_Solution/PlanMyTripApp/Controllers/UserController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanMyTripApp.Controllers
+{
+    public class UserController : Controller
+    {
+        PlanMyTripDAL.PMTRepository pmtRepo = null;
+        public UserController()
+        {
+            pmtRepo = new PlanMyTripDAL.PMTRepository();
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUser(Models.User u)
+        {
+            PlanMyTripDAL.User temp = new PlanMyTripDAL.User();
+            temp.UserId = u.UserId;
+            temp.FirstName = u.FirstName;
+            temp.EmailId = u.EmailId;
+            temp.LastName = u.LastName;
+            temp.Password = u.Password;
+            bool result = pmtRepo.AddUser(temp);
+            if (result) { ViewBag.Msg = "User Added Successfully!"; }
+            else { ViewBag.ErrorMsg = "Error occured while adding user"; }
+            return View("AddUser");
+        }
+        [HttpGet]
+        public ActionResult ViewAllUsers()
+        {
+            List<PlanMyTripDAL.User> dal = pmtRepo.ViewAll();
+            List<Models.User> mvc = new List<Models.User>();
+            foreach (var u in dal)
+            {
+                Models.User temp = new Models.User();
+                temp.UserId = u.UserId;
+                temp.FirstName = u.FirstName;
+                temp.EmailId = u.EmailId;
+                temp.LastName = u.LastName;
+                temp.Password = u.Password;
+                mvc.Add(temp);
+            }
+            return View(mvc);
+        }
+        [HttpGet]
+        public ActionResult ViewUser(int id)
+        {
+            PlanMyTripDAL.User dal = pmtRepo.View(id);
+            Models.User mvc = new Models.User();
+            mvc.UserId = dal.UserId;
+            mvc.FirstName = dal.FirstName;
+            mvc.LastName = dal.LastName;
+            mvc.EmailId = dal.EmailId;
+            mvc.Password = dal.Password;
+            return View(mvc);
+        }
+        [HttpGet]
+        public ActionResult EditUser(int id)
+        {
+            PlanMyTripDAL.User dal = pmtRepo.View(id);
+            Models.User mvc = new Models.User();
+            mvc.UserId = dal.UserId;
+            mvc.FirstName = dal.FirstName;
+            mvc.LastName = dal.LastName;
+            mvc.EmailId = dal.EmailId;
+            mvc.Password = dal.Password;
+            return View(mvc);
+        }
+        [HttpPost]
+        public ActionResult EditConfirm(Models.User mvc)
+        {
+            PlanMyTripDAL.User dal = pmtRepo.View(mvc.UserId);
+            dal.UserId = mvc.UserId;
+            dal.FirstName = mvc.FirstName;
+            dal.LastName = mvc.LastName;
+            dal.EmailId = mvc.EmailId;
+            dal.Password = mvc.Password;
+            bool result = pmtRepo.UpdateUser(dal);
+            if (result) { ViewBag.Msg = "Updated Successfully!"; }
+            else { ViewBag.ErrorMsg = "Update Failed"; }
+            return View("EditUser");
+        }
+        [HttpGet]
+        public ActionResult DeleteUser(int id)
+        {
+            PlanMyTripDAL.User dal = pmtRepo.View(id);
+            Models.User mvc = new Models.User();
+            mvc.UserId = dal.UserId;
+            mvc.FirstName = dal.FirstName;
+            mvc.LastName = dal.LastName;
+            mvc.EmailId = dal.EmailId;
+            mvc.Password = dal.Password;
+            return View(mvc);
+        }
+        [HttpPost, ActionName("DeleteUser")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            pmtRepo.DeleteUser(id);
+            return RedirectToAction("ViewAllUsers");
+        }
+        public ActionResult LoginUser()
+        {
+            return View();
+        }
+        public ActionResult LoginConfirmed(FormCollection fc)
+        {
+            int result = pmtRepo.ValidateUser(fc["EmailId"], fc["Password"]);
+            if (result>0)
+            {
+                Session["Email"] = fc["EmailId"];
+                return RedirectToAction("ViewAllUsers");
+            }
+            else
+            {
+                ViewBag.Msg = "Check Credentials!";
+                return View("LoginUser");
+            }
+        }
+        public ActionResult LogoutUser()
+        {
+            Session.Clear();
+            return View("LoginUser");
+        }
+    }
+}
---
+++ PMT_Solution/PlanMyTripDAL/PMTRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.Objects;
+namespace PlanMyTripDAL
+{
+    public class PMTRepository
+    {
+        PlanMyTripDBEntities planMyTripDBContext = null;
+        public PMTRepository()
+        {
+            planMyTripDBContext = new PlanMyTripDBEntities();
+        }
+        public PMTRepository(PlanMyTripDBEntities context)
+        {
+            planMyTripDBContext = context;
+        }
+        public bool AddUser(User userEnt)
+        {
+            planMyTripDBContext.Users.Add(userEnt);
+            int result = planMyTripDBContext.SaveChanges();
+            if (result > 0) { return true; }
+            else { return false; }
+        }
+        public bool UpdateUser(User user)
+        {
+            User tuser = planMyTripDBContext.Users.Find(user.UserId);
+            tuser.UserId = user.UserId;
+            tuser.FirstName = user.FirstName;
+            tuser.LastName = user.LastName;
+            tuser.Password = user.Password;
+            tuser.EmailId = user.EmailId;
+            int result = planMyTripDBContext.SaveChanges();
+            if (result > 0) { return true; }
+            else { return false; }
+        }
+        public bool DeleteUser(int usrId)
+        {
+            User tuser = planMyTripDBContext.Users.Find(usrId);
+            planMyTripDBContext.Users.Remove(tuser);
+            int result = planMyTripDBContext.SaveChanges();
+            if (result > 0) { return true; }
+            else { return false; }
+        }
+        public int ValidateUser(string emailId,string password)
+        {
+            List<User> result = (from c in planMyTripDBContext.Users
+                          where c.EmailId == emailId && c.Password == password select c).ToList();
+            if (result.Count > 0) { return result.Count; }
+            else { return -1; }
+        }
+        public List<User> ViewAll()
+        {
+            return planMyTripDBContext.Users.ToList();
+        }
+        public User View(int id)
+        {
+            return planMyTripDBContext.Users.Find(id);
+        }
+
+
+
+    }
+}
